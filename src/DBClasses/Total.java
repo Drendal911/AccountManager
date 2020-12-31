3504100911617a64de0e0291448b9b8c491490a7
Drendal911 <76110710+Drendal911@users.noreply.github.com>
2020-12-31 06:56:39 -0600

    Finished Withdrawal classes, updated total class

--- src/DBClasses/Total.java
+++ src/DBClasses/Total.java
@@ -1,10 +1,28 @@
 package DBClasses;
 
-public class Total {
-    int totalAmt;
+import java.sql.Date;
 
-    public Total(int totalAmt) {
+public class Total {
+    int userID;
+    int totalAmt;
+    Date date;
+    String transactionType;
+    int transactionID;
+
+    public Total(int userID, int totalAmt, Date date, String transactionType, int transactionID) {
+        this.userID = userID;
         this.totalAmt = totalAmt;
+        this.date = date;
+        this.transactionType = transactionType;
+        this.transactionID = transactionID;
+    }
+
+    public int getUserID() {
+        return userID;
+    }
+
+    public void setUserID(int userID) {
+        this.userID = userID;
     }
 
     public int getTotalAmt() {
@@ -13,5 +31,29 @@ public class Total {
 
     public void setTotalAmt(int totalAmt) {
         this.totalAmt = totalAmt;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getTransactionType() {
+        return transactionType;
+    }
+
+    public void setTransactionType(String transactionType) {
+        this.transactionType = transactionType;
+    }
+
+    public int getTransactionID() {
+        return transactionID;
+    }
+
+    public void setTransactionID(int transactionID) {
+        this.transactionID = transactionID;
     }
 }
