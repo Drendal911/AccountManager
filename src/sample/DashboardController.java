e1068607bac0fc294af1a0bee7bed23210838051
Drendal911 <76110710+Drendal911@users.noreply.github.com>
2021-01-03 10:58:24 -0600

    Link fxId's

--- src/sample/DashboardController.java
+++ src/sample/DashboardController.java
@@ -6,7 +6,8 @@ import javafx.scene.control.*;
 public class DashboardController {
     @FXML Label wTotalLabel, dTotalLabel;
     @FXML TableView wTableView, dTableView;
-    @FXML TableColumn wNumTypeColumn, wPayeeReasonColumn, wDateColumn, wAmtColumn;
+    @FXML TableColumn wNumTypeColumn, wPayeeReasonColumn, wDateColumn, wAmtColumn, dNumTypeColumn, dPayerMemoColumn,
+            dDateColumn, dAmtColumn;
     @FXML ChoiceBox columnChoiceBox, tableChoiceBox;
     @FXML Button searchButton;
     @FXML RadioButton checkingRadioButton, savingsRadioButton;
